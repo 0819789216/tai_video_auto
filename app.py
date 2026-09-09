037aeba5457a7f4d2f55fedbbb5251e199dd6361
Ngọc Én <100946584+0819789216@users.noreply.github.com>
2026-09-09 21:29:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.markdown(
     </style>
     <div class="running-banner">
         <marquee behavior="scroll" direction="left" scrollamount="5">
-            ⚠️ Dự án mới nhú có lỗi liên hệ Ngọc Én (HCNS). Chúc bạn tải video vui vẻ và thao tác mượt mà nhé!
+            ⚠️ Dự án mới nhú có lỗi liên hệ Ngọc Én (HCNS). Để dùng bản Pro vui lòng donate trà sữa size L và full topping😍!
         </marquee>
     </div>
     """,
