a8a8d0f8d53b9d7031fe7866f246bcb71cfb2798
0819789216 <100946584+0819789216@users.noreply.github.com>
2026-09-09 16:34:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if st.button("🚀 Bắt đầu tải hàng loạt", type="primary"):
         with open("text.txt", "w", encoding="utf-8") as f:
             f.write(urls_input.strip())
 
-        output_dir = "downloaded_videos"
+        output_dir = "VIDEOS"
         zip_path = "danh_sach_video_hoan_thanh.zip"
 
         if os.path.exists(output_dir):
